4ab2c60c6f4372675a626e5291cd5b3fc02bbf37
abdhulla-k <abdhullak149@gmail.com>
2022-09-29 11:54:44 +0530

    installed and used morgan, body-parser, and set view engine 'ejs'

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 const express = require( "express" );
 
+const bodyParser = require( "body-parser" );
+const morgan = require( "morgan" );
+
 const app = express();
+
+// use morgan
+app.use( morgan(':method :url :status :res[content-length] - :response-time ms') );
+
+// set view engine
+app.set( "view engine", "ejs" )
+app.set( "views", "views" );
+
+// set body-parser
+app.use( bodyParser.urlencoded({ extended: false }));
 
 app.use( '/', (req, res) => {
     res.send( "<h1>Hi</h1>" );
