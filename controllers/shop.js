9b250471189cd080a6eb7e0393f8f7393f659f00
abdhulla-k <abdhullak149@gmail.com>
2022-10-03 11:35:16 +0530

    created shop page to show all products for everyone

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,6 +1,29 @@
+const Products = require( "../models/product" );
+
 exports.getShop = ( req, res, next ) => {
     let userStatus = req.session.userLoggedIn? "true" : "";
     let userType = req.session.userType; // seller or user
 
     res.render( "user/home", { user: userStatus, userType: userType });
+}
+
+exports.showAllProducts = ( req, res, next ) => {
+    let userStatus = req.session.userLoggedIn? "true" : "";
+    let userType = req.session.userType; // seller or user
+
+    Products.find({}, ( err, data ) => {
+        if( data ) {
+            res.render( "user/shop", {
+                user: userStatus,
+                userType: userType,
+                products: data
+            });
+        } else {
+            res.render( "user/shop", {
+                user: userStatus,
+                userType: userType,
+                products: []
+            });
+        }
+    })
 }
--- routes/shop.js
+++ routes/shop.js
@@ -2,8 +2,10 @@ const express = require( "express" );
 
 const router = express.Router();
 
-const shopController = require( '../controllers/shop')
+const shopController = require( '../controllers/shop');
 
 router.get( '/', shopController.getShop );
+
+router.get('/showProducts', shopController.showAllProducts );
 
 module.exports = router;
