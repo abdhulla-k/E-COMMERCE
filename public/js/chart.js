14fc33894cbd8a7d16addde61c7f9b21c36f510f
abdhulla-k <abdhullak149@gmail.com>
2022-10-22 13:14:01 +0530

    created an area chart to show the profit of the seller

--- public/js/chart.js
+++ public/js/chart.js
@@ -85,10 +85,12 @@ $(function () {
         }
     };
     var areaData = {
-        labels: ["2013", "2014", "2015", "2016", "2017"],
+        // labels: ["2013", "2014", "2015", "2016", "2017"],
+        labels: ["10/22/33", "10, 22, 44", "10/ 33/ 88"],
         datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
+            label: 'profit',
+            // data: [12, 19, 3, 5, 2, 3],
+            data: [],
             backgroundColor: [
                 'rgba(255, 99, 132, 0.2)',
                 'rgba(54, 162, 235, 0.2)',
@@ -213,6 +215,18 @@ $(function () {
                         options: doughnutPieOptions
                     });
                 }
+
+                // area chart
+                if ($("#areaChart").length) {
+                    areaData.labels = [...res.profitDate];
+                    areaData.datasets[0].data = [...res.profit];
+                    var areaChartCanvas = $("#areaChart").get(0).getContext("2d");
+                    var areaChart = new Chart(areaChartCanvas, {
+                        type: 'line',
+                        data: areaData,
+                        options: areaOptions
+                    });
+                }
             })
 
         })
@@ -245,14 +259,14 @@ $(function () {
     //     });
     // }
 
-    if ($("#areaChart").length) {
-        var areaChartCanvas = $("#areaChart").get(0).getContext("2d");
-        var areaChart = new Chart(areaChartCanvas, {
-            type: 'line',
-            data: areaData,
-            options: areaOptions
-        });
-    }
+    // if ($("#areaChart").length) {
+    //     var areaChartCanvas = $("#areaChart").get(0).getContext("2d");
+    //     var areaChart = new Chart(areaChartCanvas, {
+    //         type: 'line',
+    //         data: areaData,
+    //         options: areaOptions
+    //     });
+    // }
 
     if ($("#scatterChart").length) {
         var scatterChartCanvas = $("#scatterChart").get(0).getContext("2d");
